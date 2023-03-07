703844f8c1090984162592efd8016534d2f2db69
“laurabpaulsen” <202005791@post.au.dk>
2023-03-07 15:50:26 +0100

    plot condition decoding

---
+++ decoding/condition/plot_results.py
@@ -0,0 +1,18 @@
+import sys
+import pathlib
+sys.path.append(str(pathlib.Path(__file__).parents[2]))
+from utils.analysis import plot
+from utils.analysis.tools import chance_level
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+if __name__ == "__main__":
+    acc = np.load(os.path.join('accuracies', 'accuracies_memory_or_visual.npy'), allow_pickle=True)
+    print(acc.shape)
+
+    # one fig with the average
+    plt = plot.plot_tgm_fig(acc, vmin=20, vmax=80, chance_level=chance_level(13190, alpha = 0.001, p = 0.5))
+    plt.savefig(os.path.join('plots', f'tgm.png'))
+    
+    
