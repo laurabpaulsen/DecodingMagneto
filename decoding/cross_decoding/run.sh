3cc50383bd75b74718a1d30416c0724a1fecf3db
“laurabpaulsen” <202005791@post.au.dk>
2023-03-16 15:52:10 +0100

    automation script all parc

---
+++ decoding/cross_decoding/run.sh
@@ -0,0 +1,5 @@
+python cross_decoding.py --parc 'aparc' --ncores 10
+python cross_decoding.py --parc 'aparc.a2009s' --ncores 10
+python cross_decoding.py --parc 'sens' --ncores 10
+python cross_decoding.py --parc 'aparc.DKTatlas' --ncores 10
+python cross_decoding.py --parc 'HCPMMP1' --ncores 10
