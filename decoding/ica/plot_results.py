3bbcf5e2298f34e84fae42d46edd01bc4cb54633
“laurabpaulsen” <202005791@post.au.dk>
2023-03-21 15:03:37 +0100

    split into functions

--- decoding/ica/plot_results.py
+++ decoding/ica/plot_results.py
@@ -10,9 +10,29 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 alpha = 0.05
 
+def session_plots_individual(accuracies: dict, alpha: float = 0.05, n_trials: int = 588):
+    """
+    Plots the results of the TGM decoding for each session individually.
+    
+    Parameters
+    ----------
+    accuracies: dict
+        Dictionary containing the accuracies for each session.
+    alpha: float
+        Alpha level for the chance level.
+    n_trials: int
+        Number of trials in the session.
+    """
+    for f, acc in accuracies.items():
+        plt = plot.plot_tgm_fig(acc, vmin=20, vmax=80, chance_level=chance_level(588, alpha = alpha, p = 0.5))
+        plt.tight_layout()
+
+        out_path = os.path.join('plots', f'tgm_{f}.png')
+        plt.savefig(out_path)
+        plt.close()
 
 
-if __name__ == "__main__":
+def main_plot_generator():
     # list all files in the folder
     files = os.listdir('accuracies')
 
@@ -24,33 +44,29 @@ if __name__ == "__main__":
         acc = np.load(os.path.join('accuracies', f), allow_pickle=True)
         accuracies[f[:-4]] = acc
 
+    # plot all sessions
+    session_plots_individual(accuracies)
     
-    # individual plots
-    for f, acc in accuracies.items():
-        # one fig with the average
-        plt = plot.plot_tgm_fig(acc, vmin=20, vmax=80, chance_level=chance_level(588, alpha = alpha, p = 0.5))
-        plt.tight_layout()
+    # Averaged over sessions
+    # two subplots one for ica and one for no ica
+    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
 
-        out_path = os.path.join('plots', f'tgm_{f[:-4]}.png')
-        plt.savefig(out_path)
-
-    # average plot of all with no_ica
+    # with no_ica
     no_ica = np.array([acc for f, acc in accuracies.items() if 'no_ica' in f])
-    print(no_ica)
     no_ica = np.mean(no_ica, axis=0)
-    plt = plot.plot_tgm_fig(no_ica, vmin=20, vmax=80, chance_level=chance_level(588, alpha = alpha, p = 0.5))
-    plt.tight_layout()
+    ax[0] = plot.plot_tgm_ax(no_ica, ax[0], vmin=20, vmax=80, chance_level=chance_level(588, alpha = alpha, p = 0.5))
+    ax[0].set_title('No ICA')
 
-    out_path = os.path.join('plots', f'tgm_no_ica.png')
-    plt.savefig(out_path)
-
-    # average plot of all with ica
+    # with ica
     ica = np.array([acc for f, acc in accuracies.items() if not 'no_ica' in f])
-    print(ica)
     ica = np.mean(ica, axis=0)
-    plt = plot.plot_tgm_fig(ica, vmin=20, vmax=80, chance_level=chance_level(588, alpha = alpha, p = 0.5))
+    ax[1] = plot.plot_tgm_ax(ica, ax[1], vmin=20, vmax=80, chance_level=chance_level(588, alpha = alpha, p = 0.5))
+    ax[1].set_title('ICA')
+
     plt.tight_layout()
 
-    out_path = os.path.join('plots', f'tgm_ica.png')
+    out_path = os.path.join('plots', f'average_ica_no_ica.png')
     plt.savefig(out_path)
 
+if __name__ == "__main__":
+    main_plot_generator()
