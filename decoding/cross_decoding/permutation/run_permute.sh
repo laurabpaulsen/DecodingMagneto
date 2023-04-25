c3b6c95b224414e16bbcd1447ede4218851a7e38
laurabpaulsen <202005791@post.au.dk>
2023-04-25 11:55:16 +0200

    init bash script for running permutation tests

---
+++ decoding/cross_decoding/permutation/run_permute.sh
@@ -0,0 +1,7 @@
+python3 cluster_permutation.py --train1 vis --test1 mem --train2 mem --test2 mem
+python3 cluster_permutation.py --train1 vis --test1 vis --train2 mem --test2 mem
+python3 cluster_permutation.py --train1 vis --test1 mem --train2 mem --test2 vis
+python3 cluster_permutation.py --train1 vis --test1 vis --train2 mem --test2 vis
+python3 cluster_permutation.py --train1 mem --test1 mem --train2 mem --test2 vis
+python3 cluster_permutation.py --train1 vis --test1 vis --train2 vis --test2 mem
+
