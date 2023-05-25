510f6473ffc3100c5a197a1cd2918ec03a11330c
“laurabpaulsen” <202005791@post.au.dk>
2023-05-25 13:25:39 +0200

    update average ICA plot

--- decoding/ica/plot_results.py
+++ decoding/ica/plot_results.py
@@ -12,7 +12,7 @@ alpha = 0.001
 
 
 # set parameters for all plots
-plt.rcParams['font.family'] = 'times new roman'
+plt.rcParams['font.family'] = 'Sans serif'
 plt.rcParams['image.cmap'] = 'RdBu_r'
 plt.rcParams['image.interpolation'] = 'bilinear'
 plt.rcParams['axes.labelsize'] = 14
@@ -59,7 +59,7 @@ def main_plot_generator():
         accuracies[f[:-4]] = acc
 
     # plot all sessions
-    session_plots_individual(accuracies)
+    #session_plots_individual(accuracies)
     
     # Averaged over sessions
     # two subplots one for ica and one for no ica
@@ -69,19 +69,19 @@ def main_plot_generator():
     no_ica = np.array([acc for f, acc in accuracies.items() if 'no_ica' in f])
     no_ica = np.mean(no_ica, axis=0)
     ax[0] = plot.plot_tgm_ax(no_ica, ax[0], vmin=20, vmax=80, chance_level=chance_level(588*11, alpha = alpha, p = 0.5))
-    ax[0].set_title('No ICA')
+    ax[0].set_title('No ICA'.upper())
 
     # with ica
     ica = np.array([acc for f, acc in accuracies.items() if not 'no_ica' in f])
     ica = np.mean(ica, axis=0)
     ax[1] = plot.plot_tgm_ax(ica, ax[1], vmin=20, vmax=80, chance_level=chance_level(588*11, alpha = alpha, p = 0.5))
-    ax[1].set_title('ICA')
+    ax[1].set_title('ICA'.upper())  
     
     # add subplot for colorbar
     divider = make_axes_locatable(ax[1])
     cax = divider.append_axes("right", size="5%", pad=0.05)
     cbar = plt.colorbar(ax[1].images[0], cax=cax)
-    cbar.set_label('Accuracy (%)')
+    cbar.set_label('Accuracy (%)'.upper())
 
     # making sure to align the plots correctly after adding the colorbar to the other axis
     divider = make_axes_locatable(ax[0])
