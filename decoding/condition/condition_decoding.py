a7afc10a8d832e77625a4d1f9e073c5be20bff4a
“laurabpaulsen” <202005791@post.au.dk>
2023-10-02 10:56:20 +0200

    move analysis to sensor space

--- decoding/condition/condition_decoding.py
+++ decoding/condition/condition_decoding.py
@@ -1,5 +1,5 @@
 """
-Notes: Do we want to do this in source or sensor space?
+
 """
 import os
 import numpy as np
@@ -7,57 +7,32 @@ import pathlib
 import sys
 sys.path.append(str(pathlib.Path(__file__).parents[2])) # adds the parent directory to the path so that the utils module can be imported
 
-
 from utils.analysis.decoder import Decoder
-from utils.data.triggers import balance_class_weights
-from utils.data.concatenate import flip_sign
-import pickle as pkl
-from tqdm import tqdm
+from utils.data.triggers import balance_class_weights, get_triggers_equal
+from utils.data.concatenate import read_and_concate_sessions
 
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
-def read_and_concat(file_list):
-    for f in tqdm(file_list):
-        # get session number and type
-        session = f.split('/')[-1]
-        session = session.split('_parcelled')[0]
-
-        # load y 
-        with open(os.path.join('..', '..', 'info_files', 'events', f'{session}_events.pkl'), 'rb') as y_f:
-            y_tmp = pkl.load(y_f)
-
-        X_tmp = np.load(f)
-        X_tmp = X_tmp.transpose(2, 0, 1)
-
-        if file_list.index(f) == 0:
-            X = X_tmp
-            y = y_tmp
-        else:
-            # sign flip
-            X_tmp = flip_sign(X, X_tmp)
-
-            X = np.concatenate((X, X_tmp), axis=1)
-            y = np.concatenate((y, y_tmp))
-
-    return X, y
-
 if __name__ in "__main__":
     # list all files in the aparc.a2009s directory
-    path = os.path.join(os.path.sep, 'media', '8.1', 'final_data', 'laurap', 'source_space', 'parcelled', 'aparc.a2009s')
+    path = os.path.join(os.path.sep, 'media', '8.1', 'final_data', 'laurap', 'epochs')
     files = os.listdir(path)
+    
+    # remove no_ica files
+    files = [f for f in files if 'no_ica' not in f]
 
-    memory_files = [os.path.join(path, f) for f in files if 'memory' in f]
-    visual_files = [os.path.join(path, f)for f in files if 'visual' in f]
+    memory_files = [f for f in files if 'memory' in f]
+    visual_files = [f for f in files if 'visual' in f]
 
     # load data
     logging.info('Loading data...')
-    X_memory, y_memory = read_and_concat(memory_files)
-    X_visual, y_visual = read_and_concat(visual_files)
+    X_memory, y_memory = read_and_concate_sessions(memory_files, get_triggers_equal())
+    X_visual, y_visual = read_and_concate_sessions(visual_files, get_triggers_equal())
 
     # concatenate data
     X = np.concatenate((X_memory, X_visual), axis=1)
-    y = [1] * len(y_memory) + [0] * len(y_visual)
+    y = np.array([1] * len(y_memory) + [0] * len(y_visual))
 
     # shuffle indices
     indices = np.arange(X.shape[1])
