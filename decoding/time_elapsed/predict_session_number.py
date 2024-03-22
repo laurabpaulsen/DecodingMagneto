534417508cdae4cbddedd754b76351349e09c608
laurabpaulsen <202005791@post.au.dk>
2024-03-22 10:00:05 +0100

    scripts for prediction

--- decoding/time_elapsed/predict_session_number.py
+++ decoding/time_elapsed/predict_session_number.py
@@ -93,13 +93,13 @@ if __name__ == '__main__':
     args = parse_args()
 
     path = Path(__file__)
-    output_path = path.parent / "results" / f'{args.trial_type}_{args.task}_session_day.pkl'
+    output_path = path.parent / "results" / f'{args.trial_type}_{args.task}_session_number.pkl'
 
     # ensure that the results directory exists
     output_path.parents[0].mkdir(parents=True, exist_ok=True)
     
     sessions = [['visual_03', 'visual_04'], ['visual_05', 'visual_06', 'visual_07'], ['visual_08', 'visual_09', 'visual_10'], ['visual_11', 'visual_12', 'visual_13'], ['memory_01', 'memory_02'], ['memory_03', 'memory_04', 'memory_05', 'memory_06'],  ['memory_07', 'memory_08', 'memory_09', 'memory_10', 'memory_11'], ['memory_12', 'memory_13', 'memory_14', 'memory_15'] ,['visual_14', 'visual_15', 'visual_16', 'visual_17', 'visual_18', 'visual_19'],['visual_23', 'visual_24', 'visual_25', 'visual_26', 'visual_27', 'visual_28', 'visual_29'],['visual_30', 'visual_31', 'visual_32', 'visual_33', 'visual_34', 'visual_35', 'visual_36', 'visual_37', 'visual_38']]
-    session_days = [0, 1, 7, 8, 14, 21, 35, 36, 145, 159, 161]
+    session_days = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     
     #  change sessions depending on the task
     if args.task == 'visual':
